7993f7cb1f7cb943b14de1f124832b50b4ee7950
Roma <r-ghost@inbox.ru>
2022-02-27 03:41:46 +0300

    Modal

--- js/main.js
+++ js/main.js
@@ -92,4 +92,46 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadLine);
+    
+    
+    // modal
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.classList.add('show')
+                modal.classList.remove('hide')
+                document.body.style.overflow = 'hidden'
+            })
+
+        })
+
+        function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = ''
+        }
+        
+
+        
+        modalClose.addEventListener('click' , closeModal)
+
+        modal.addEventListener('click' , (e) => {
+            if(e.target === modal) {
+               closeModal();
+            }
+        })
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === "Escape"){
+                closeModal();
+            }
+        })
+
+
 })
+
+ 
+
